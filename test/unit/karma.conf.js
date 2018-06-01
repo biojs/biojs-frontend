a163b5d811cfa3b06ba0b77b49cb766ba4eff343
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-06-01 18:47:37 +0530

    Changed browser in Karma to Chrome

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -11,10 +11,10 @@ module.exports = function karmaConfig (config) {
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
-    files: ['./index.js'],
+    files: ['../../node_modules/es6-promise/dist/es6-promise.auto.js', './index.js'],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
