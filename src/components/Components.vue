db7ec12e265ceb5bec1e5360c6a6ad77229ca9ab
Sarthak Sehgal <sarthaksehgal00@gmail.com>
2018-06-10 14:04:55 +0530

    Corrected information in component table.

--- src/components/Components.vue
+++ src/components/Components.vue
@@ -41,9 +41,9 @@
 			</div>
 		</div>
 		<div id="topComponents">
-			<component-table title="Top Downloads" components="most_recent" />
+			<component-table title="Top Downloads" components="top_dwnld" />
 			<component-table title="Top Starred" components="top_starred" />
-			<component-table title="Most Recent" components="top_dwnld" />
+			<component-table title="Most Recent" components="most_recent" />
 		</div>
 	</div>
 </div>
