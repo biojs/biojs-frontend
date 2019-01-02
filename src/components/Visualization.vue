b75f10834c30da58ab1c9742d7733982bfebf791
Dennis Schwartz <dennis@repositive.io>
2019-01-02 23:26:58 +0100

    Simpler script fix by replacing require

--- src/components/Visualization.vue
+++ src/components/Visualization.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="visualization">
-    {{ name }}@{{ version }}
-    <div id="loading-bar-spinner" v-if="this.loading" class="spinner">
-      <div class="spinner-icon"></div>
-    </div>
-	<!-- <iframe srcdoc="{{ this.buildVisualisationScript() }}"></iframe> -->
+  	{{ name }}@{{ version }}
+    <div id="mount"></div>
   </div>
 </template>
 <script>
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
+import Vue from 'vue';
+import VisFrame from './VisFrame.vue';
 const BUILDER_URL = 'http://localhost:3000';
 export default {
 	data () {
@@ -49,9 +48,14 @@ export default {
 			this.bundle = data;
 			console.log(`Got bundled script for ${this.name}!`);
 			console.log(data.slice(0, 100));
-			this.buildVisualisationScript()
-				.then(this.addVisualisation);
-			// this.renderVisualisation();
+			this.buildVisualisationScript().then(script => {
+				const FrameConstructor = Vue.extend(VisFrame);
+				new FrameConstructor({
+					propsData: {
+						snippet: script
+					}
+				}).$mount('#mount');
+			});
 		});
 	},
 	methods: {
@@ -69,21 +73,6 @@ export default {
 		getSnippetCode (url) {
 			return axios.get(url).then(res => res.data);
 		},
-		addSnippetCSS () {
-			return Promise.all(
-				this.css.map(
-					css =>
-						new Promise((resolve, reject) => {
-							const style = document.createElement('link');
-							style.rel = 'stylesheet';
-							style.type = 'text/css';
-							style.href = css.css_url;
-							style.onload = resolve;
-							document.head.appendChild(style);
-						})
-				)
-			);
-		},
 		createCSSImport (script, css) {
 			return (
 				script +
@@ -93,103 +82,46 @@ export default {
 		createJSImport (script, js) {
 			return script + `<script src="${js.js_url}"><\/script>\n`;
 		},
-		addSnippetJS () {
-			return Promise.all(
-				this.js.map(
-					js =>
-						new Promise((resolve, reject) => {
-							const script = document.createElement('script');
-							script.setAttribute('src', js.js_url);
-							script.onload = resolve;
-							document.head.appendChild(script);
-						})
-				)
-			);
-		},
 		fixSnippetCode (code) {
 			// detect rootDiv
 			code = code.replace(/yourDiv|mainDiv|masterDiv|biojsDiv/g, 'rootDiv');
 			// detect component var name
-			const varNamePattern = new RegExp(
-				"(var|let|const)\\s*(\\S+)\\s*=\\s*require\\([\"']" +
-          this.name +
-          "[\"']\\);?"
-			);
-			const varNameMatch = code.match(varNamePattern);
-			if (!varNameMatch) throw new Error('No import found!');
-			const varName = varNameMatch[2];
-			// delete require line
-			code = code.replace(varNameMatch[0], '');
-			// replace instantiation with window[] call
-			const instanceCallPattern = new RegExp('(new\\s+)?' + varName + '\\(');
+			const importPattern = new RegExp('require\\(["\']' + this.name + '["\']\\);');
 			code = code.replace(
-				instanceCallPattern,
-				"new window['" + this.name + "']("
+				importPattern,
+				"window['" + this.name + "'];"
 			);
 			return code;
 		},
-		renderVisualisation () {
-			return this.addSnippetCSS()
-				.then(this.addSnippetJS)
-				.then(() => this.getSnippetCode(this.snippetURL))
-				.then(code => {
-					this.loading = false;
-					// eslint-disable-next-line
-          var rootDiv = document.getElementById("snippetDiv");
-					// TODO: Serve bundle js statically after compilation?
-					// eslint-disable-next-line
-          eval(this.bundle); // Add bundle code
-					const newSnippet = this.fixSnippetCode(code);
-					eval(newSnippet); // run snippet code
-					console.log(newSnippet);
-				});
-		},
-		addVisualisation (script) {
-			const iframe = document.createElement('iframe');
-			iframe.setAttribute('srcdoc', script);
-			const visDiv = document.getElementById('visualization');
-			visDiv.appendChild(iframe);
-		},
 		buildVisualisationScript () {
-			return this.getSnippetCode(this.snippetURL)
-				.then(code => {
-					const newSnippetCode = this.fixSnippetCode(code);
-					// Create HTML boilerplate
-					let script = '<!DOCTYPE html>\n<html>\n<head>\n';
-					// Add CSS
-					script = this.css.reduce(this.createCSSImport, script);
-					// Add JS deps
-					script = this.js.reduce(this.createJSImport, script);
-					// Add bundle
-					script += `<script>\n${this.bundle}\n<\/script>\n`;
-					// Add snippet script
-					script += `</head>\n<body>\n`;
-					script += '<div id="snippetDiv"></div>\n'
-					script += `<script>\nvar rootDiv = document.getElementById('snippetDiv');\n`
-					script += `${newSnippetCode}\n<\/script>\n`;
-					script += '</body>\n</html>\n';
-					console.log(script.substr(-1000));
-					// return result
-					return script
-					// this.addVisualisation(script);
-				});
+			return this.getSnippetCode(this.snippetURL).then(code => {
+				const newSnippetCode = this.fixSnippetCode(code);
+				// Create HTML boilerplate
+				let script = '<!DOCTYPE html>\n<html>\n<head>\n';
+				// Add CSS
+				script = this.css.reduce(this.createCSSImport, script);
+				// Add JS deps
+				script = this.js.reduce(this.createJSImport, script);
+				// Add bundle
+				script += `<script>\n${this.bundle}\n<\/script>\n`;
+				// Add snippet script
+				script += `</head>\n<body>\n`;
+				script += '<div id="snippetDiv"></div>\n';
+				script += `<script>\nvar rootDiv = document.getElementById('snippetDiv');\n`;
+				script += `${newSnippetCode}\n<\/script>\n`;
+				script += '</body>\n</html>\n';
+				// return result
+				return script;
+			});
 		}
 	}
 };
 </script>
 <style lang="scss" scoped>
-#visualization {
+#mount {
   margin: 30px 0;
   width: 100%;
   height: 500px;
-}
-#frame {
-  width: 100%;
-  height: 100%;
-  box-shadow: 0 0 2px 0 #cecece;
-  outline: none;
-  border: 1px solid #eee;
-  border-radius: 5px;
 }
 .message {
   height: 60px;
@@ -197,33 +129,4 @@ export default {
   display: none;
 }
 
-#loading-bar-spinner.spinner {
-  left: 50%;
-  margin-left: -20px;
-  top: 50%;
-  margin-top: -20px;
-  position: absolute;
-  z-index: 19 !important;
-  animation: loading-bar-spinner 400ms linear infinite;
-}
-
-#loading-bar-spinner.spinner .spinner-icon {
-  width: 60px;
-  height: 60px;
-  border: solid 4px transparent;
-  border-top-color: rgb(0, 126, 58) !important;
-  border-left-color: rgb(0, 126, 58) !important;
-  border-radius: 50%;
-}
-
-@keyframes loading-bar-spinner {
-  0% {
-    transform: rotate(0deg);
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-    transform: rotate(360deg);
-  }
-}
 </style>
